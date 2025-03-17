1eb6532b341c3fec16b0bc7c68efc794f9514485
aran truslove <at2128@cam.ac.uk>
2025-03-17 16:06:27 +0000

    Converting data to torch type.

--- m2_utilities/load_data.py
+++ m2_utilities/load_data.py
@@ -1,4 +1,4 @@
-import numpy as np
+import torch
 import h5py
 
 
@@ -6,4 +6,7 @@ def load_trajectories(data_path):
     with h5py.File(data_path, "r") as f:
         # Access the trajectories
         trajectories = f["trajectories"][:]
+
+        # Converting to a torch tensor
+        trajectories = torch.from_numpy(trajectories)
     return trajectories
