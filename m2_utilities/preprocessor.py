2f73b42931558f1a5409cac80b2d656d47dcd97f
aran truslove <at2128@cam.ac.uk>
2025-03-17 17:44:44 +0000

    Initial full version of preprocessor.py.

--- m2_utilities/preprocessor.py
+++ m2_utilities/preprocessor.py
@@ -1,3 +1,11 @@
+import torch
+from transformers import AutoTokenizer
+
+# Initialising the tokenizer
+MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+
+
 def string_dp(val, decimals):
     """Convert a float to a string with a fixed number of decimal places."""
     return f"{val:.{decimals}f}"
@@ -9,29 +17,44 @@ def stringify(trajectories, decimals):
     values. Uses semicolons to separate between time points.
     """
     # Convert to strings with a fixed number of dcimal places
-    trajectories_str = []
+    stringified = []
     for trajectory in trajectories:
         trajectory_str = ""
-        for predator, prey in trajectory:
+        for pred, prey in trajectory:
             trajectory_str += (
-                string_dp(predator, decimals) + "," + string_dp(prey, decimals) + ";"
+                string_dp(pred, decimals) + "," + string_dp(prey, decimals) + ";"
             )
 
         # Remove final semicolon and add to the list
-        trajectories_str.append(trajectory_str[:-1])
+        stringified.append(trajectory_str[:-1])
 
     # Remove the final semicolon and return
-    return trajectories_str
+    return stringified
 
 
-def encode(trajectories, alpha, decimals):
-    """
-    Encode the trajectories by scaling and rounding the trajectories into a string
-    representation.
-    """
+def destringify(stringified):
+    """Convert string format predator and prey positions to numerical format."""
+    destringified = []
+    for str_trajectory in stringified:
+        num_trajectory = []
+        for elem in str_trajectory.split(";"):
+            pred, prey = elem.split(",")
+            pred, prey = float(pred), float(prey)
+            num_trajectory.append([pred, prey])
+        destringified.append(num_trajectory)
 
-    # Scale trajectories by alpha
-    scaled_trajectories = trajectories / alpha
-    str_trajectories = stringify(scaled_trajectories, decimals)
+    return torch.tensor(destringified)
 
-    return str_trajectories
+
+def tokenize(trajectories, decimals):
+    """Convert numerical trajectories to Qwen2.5 token representations."""
+    stringified = stringify(trajectories, decimals)
+    tokens = tokenizer(stringified, return_tensors="pt")["input_ids"]
+    return tokens
+
+
+def detokenize(tokens):
+    """Convert Qwen2.5 tokens to numerical trajectories."""
+    stringified = tokenizer.batch_decode(tokens, skip_special_tokens=True)
+    num_trajectories = destringify(stringified)
+    return num_trajectories
