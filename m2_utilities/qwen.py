079a02bd6e614607dac47f4ae8f6dfc29c58d0ab
aran truslove <at2128@cam.ac.uk>
2025-03-19 16:05:26 +0000

    Adding initial function to train model.

--- m2_utilities/qwen.py
+++ m2_utilities/qwen.py
@@ -1,5 +1,7 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM
+from accelerate import Accelerator
+from tqdm import tqdm
 from m2_utilities.preprocessor import get_tokenizer
 
 
@@ -20,3 +22,44 @@ def load_qwen():
     model.lm_head.bias.requires_grad = True
 
     return model, tokenizer
+
+
+def train(model, train_loader, val_loader, max_steps=10000, learning_rate=1e-5):
+
+    optimizer = torch.optim.Adam(
+        (p for p in model.parameters() if p.requires_grad), lr=learning_rate
+    )
+    # Prepare components with Accelerator
+    accelerator = Accelerator()
+    model, optimizer, train_loader, val_loader = accelerator.prepare(
+        model, optimizer, train_loader, val_loader
+    )
+
+    steps = 0
+    while steps < max_steps:
+        # Train
+        model.train()
+        progress_bar = tqdm(train_loader, desc=f"Steps {steps}")
+        for (batch,) in progress_bar:
+
+            optimizer.zero_grad()
+            outputs = model(batch, labels=batch)
+            loss = outputs.loss
+            accelerator.backward(loss)
+            optimizer.step()
+            steps += 1
+
+            progress_bar.set_postfix(loss=loss.item())
+            if steps > max_steps:
+                break
+
+        # Evaluate on validation set
+        model.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for (batch,) in val_loader:
+                outputs = model(batch, labels=batch)
+                loss = outputs.loss
+                val_loss += loss.item()
+
+        print(f"Validation Loss: {val_loss / len(val_loader)}")
